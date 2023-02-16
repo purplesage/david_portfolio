a135635992293662dc0f7e0a5aeaa717618d3a83
purpleSage <hectormza21@gmail.com>
2023-02-16 18:10:49 -0400

    ...

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,7 @@ const { withAnimations } = require('animated-tailwindcss');
 
 module.exports = withAnimations({
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	mode: 'jit',
 	theme: {
 		extend: {
 			fontFamily: {
@@ -13,15 +14,3 @@ module.exports = withAnimations({
 	},
 	plugins: [require('tailwindcss-intersect')],
 });
-// module.exports = {
-// 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-// 	theme: {
-// 		extend: {
-// 			fontFamily: {
-// 				poppins: ['Poppins', 'sans-serif'],
-// 				sed: ['Sedgwick Ave Display', 'cursive'],
-// 			},
-// 		},
-// 	},
-// 	plugins: [require('tailwindcss-intersect')],
-// };
